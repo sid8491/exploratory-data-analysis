075a253c87f304a742fefa0d9f5d4a4800517686
sid8491 <sid.8491@gmail.com>
2016-05-02 23:15:22 +0530

    added alternatives for plot3

---
+++ plot3_alt.R
@@ -0,0 +1,20 @@
+# initialize ggplot2
+library(ggplot2)
+
+# get the data 
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# get total emissions from different years of Baltimore City, Maryland
+NEI.baltimore <- subset(NEI, fips == "24510")
+NEI.type <- aggregate(Emissions ~ year + type, NEI.baltimore, sum)
+
+# plot the graph
+png("plot3_alt.png", 640, 400)
+g <- ggplot(NEI.type, aes(factor(year), Emissions, fill = type))
+g <- g + geom_bar(stat = "identity", position = position_dodge()) + 
+  facet_wrap(~type, ncol = 2) +
+  xlab("Year") + ylab(expression ("Total" ~ PM[2.5] ~"Emissions (tons)")) + 
+  ggtitle("Baltimore City" ~ PM[2.5] ~ " Emmission by Source type and Year")
+print(g)
+dev.off()
